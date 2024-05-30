0e70b1bcdf84303be04cec4dda7a2532b99b9309
C1333760 Abel Cabral De Arruda <c1333760@interno.bb.com.br>
2024-05-30 05:25:07 -0300

    copiando html da tab ativa e copiando url

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import Index from '../components/Index';
-import New from '../components/New';
+import React, { useState, useEffect } from "react";
+import Index from "../components/Index";
+import New from "../components/New";
 
 export default function Home() {
-  const [activePage, setActivePage] = useState('index');
+  const [activePage, setActivePage] = useState("index");
   const [currentUrl, setCurrentUrl] = useState(null);
+  const [pageText, setPageText] = useState(null);
 
   useEffect(() => {
-    const getCurrentTabUrl = () => {
-      chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-        const currentTab = tabs[0];
-        const url = currentTab.url;
-        setCurrentUrl(url);
-      });
-    };
+    chrome.runtime.onMessage.addListener((message) => {
+      if(message.pageText) {
+        setPageText(message.pageText);
+      }
+    })
 
-    try {
-      getCurrentTabUrl();
-    } catch (error) {
-      setCurrentUrl('http://localhost:3000');
-    }
-   
-    // Remover o listener quando o componente for desmontado
-    return () => {
-      chrome.runtime.onMessage.removeListener(handleMessage);
-    };
-  }, []);
+    // Envia uma mensagem para o script de fundo indicando que o componente Home foi montado
+    chrome.runtime.sendMessage({ popupOpened: true });
+
+    // Obtém a URL da aba ativa e armazena no estado
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      const currentTab = tabs[0];
+      const url = currentTab.url;
+      setCurrentUrl(url);
+    });
+  }, []); // Executa apenas uma vez quando o componente é montado
 
   const navigateToPage = (page) => {
     setActivePage(page);
@@ -33,10 +31,12 @@ export default function Home() {
 
   return (
     <>
-      {activePage === 'index' && <Index navigateToPage={navigateToPage} />}
-      {activePage === 'new' && <New navigateToPage={navigateToPage} />}
+      {activePage === "index" && <Index navigateToPage={navigateToPage} />}
+      {activePage === "new" && <New navigateToPage={navigateToPage} />}
       {/* Exibir a URL da página atual */}
       <p>URL da página atual: {currentUrl}</p>
+      {/* Exibir o texto da página atual */}
+      <p>Texto da página atual: {pageText}</p>
     </>
   );
 }
--- public/background.js
+++ public/background.js
@@ -1,5 +1,78 @@
-chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    const currentTab = tabs[0];
-    const currentUrl = currentTab.url;
-    chrome.runtime.sendMessage({ url: currentUrl });
-});
+// chrome.action.onClicked.addListener(function(tab) {
+//     console.log('Click')
+//     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+//         const currentTab = tabs[0];
+//         const currentUrl = currentTab.url;
+//         chrome.tabs.sendMessage({ url: currentUrl });
+//         console.log('enviou')
+//     });
+// });
+
+// chrome.runtime.onInstalled.addListener(function() {
+//     console.log('Extensão instalada ou atualizada.');
+//   });
+
+// Escuta mensagens enviadas pelo popup
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.popupOpened) {
+      console.log("Popup aberto!");
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        const activeTab = tabs[0];
+        const tabId = activeTab.id;
+  
+        chrome.scripting.executeScript(
+          {
+            target: { tabId: tabId },
+            func: () => document.documentElement.innerText,
+          },
+          (results) => {
+            if (chrome.runtime.lastError) {
+              console.error(chrome.runtime.lastError.message);
+              sendResponse({ pageText: null });
+            } else if (results && results[0].result) {
+              chrome.runtime.sendMessage({ pageText: results[0].result });
+            } else {
+              console.error("Texto da página não recebido.");
+              sendResponse({ pageText: null });
+            }
+          }
+        );
+  
+        // Return true to indicate that the response will be sent asynchronously
+        return true;
+      });
+    }
+  });
+
+// chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+//     const currentTab = tabs[0];
+//     const currentUrl = currentTab.url;
+//     chrome.runtime.sendMessage({ url: currentUrl });
+// });
+
+// // Função para lidar com a resposta do script injetado
+// function handleScriptResponse() {
+//     // const textFromPage = result[0];
+//     // Envia uma mensagem para o popup.js com o conteúdo do texto
+//     chrome.runtime.sendMessage({ text: 'Ahhhhhhhh' });
+// }
+
+// // Função para injetar um script na página ativa e obter o texto do HTML
+// function getTextFromPage() {
+//     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+//         const tab = tabs[0];
+//         chrome.scripting.executeScript({
+//             target: { tabId: tab.id },
+//             function: () => {
+//                 // Obtém o texto do HTML da página
+//                 const text = document.documentElement.innerText;
+//                 return text;
+//             }
+//         }, handleScriptResponse);
+//     });
+// }
+
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//     // Envia uma mensagem para o popup quando a extensão for clicada e aberta
+//     chrome.runtime.sendMessage({ message: "Extensão clicada e aberta!" });
+// });
