7ed322c8402d3de3bba83f9726e7826d79842f8e
C1333760 Abel Cabral De Arruda <c1333760@interno.bb.com.br>
2024-06-10 10:02:28 -0300

    (feat): criou cache local

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -1,15 +1,15 @@
-import styles from './Cards.module.css';
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import Chip from '@mui/material/Chip';
-import IconButton from '@mui/material/IconButton';
-import DeleteIcon from '@mui/icons-material/Delete';
-import Backdrop from '@mui/material/Backdrop';
-import CircularProgress from '@mui/material/CircularProgress';
+import styles from "./Cards.module.css";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Chip from "@mui/material/Chip";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const config = {
   headers: {
@@ -37,7 +37,7 @@ export default function Cards(props) {
 
   const buscarImagem = async (tag1, tag2, tag3) => {
     try {
-      const queries = [tag1, tag2, tag3, 'tecnologia'];
+      const queries = [tag1, tag2, tag3, "tecnologia"];
       for (const query of queries) {
         const response = await axios.get(
           `https://api.unsplash.com/search/photos?query=${query}&orientation=landscape&per_page=1&client_id=Z98UiqP-pTJ3779KAb3UbnNhfy_qqXApYGozFZcYoXc`
@@ -51,34 +51,52 @@ export default function Cards(props) {
     }
   };
 
-  const carregarCards = async () => {
-    setLoadingData(true);
+  const carregarCards = () => {
+    // Carrega o Cache no navegador
+    const cache = getCachedData()
+    if (cache) {
+      setTagsGerais(cache);
+    }
+
+    // Inicia busca por atualizacoes
     try {
-      const response = await axios.get(
-        'https://websage-api.abelcode.dev/api/list-items',
+      axios.get(
+        "https://websage-api.abelcode.dev/api/list-items",
         config
-      );
-      setTagsGerais(response.data);
+      ).then(response => {
+        setTagsGerais(response.data);
+
+        // Armazenar ou atualiza os dados no localStorage
+        localStorage.setItem("cards", JSON.stringify({ data: response.data }));
+      })
     } catch (err) {
       console.log(err);
     }
-    setLoadingData(false);
   };
 
   const adicionarCard = (url, statusBotao) => {
     statusBotao(true);
-  
-    axios.post(`https://websage-api.abelcode.dev/api/save-item`, { url }, config)
-      .then(async novoCard => {
+
+    axios
+      .post(`https://websage-api.abelcode.dev/api/save-item`, { url }, config)
+      .then(async (novoCard) => {
         const novoCardData = novoCard.data;
-        
+
         try {
-          const imageUrl = await buscarImagem(novoCardData.tag1, novoCardData.tag2, novoCardData.tag3);
+          const imageUrl = await buscarImagem(
+            novoCardData.tag1,
+            novoCardData.tag2,
+            novoCardData.tag3
+          );
           adicionarNovoElemento(novoCardData, imageUrl);
           return { novoCardData, imageUrl };
         } catch (error) {
-          console.log(error.message)
-          return { novoCardData, imageUrl: 'https://images.unsplash.com/photo-1557724630-96de91632c3b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2MTg2MTd8MHwxfHNlYXJjaHwxfHx1bmRlZmluZWR8ZW58MHwwfHx8MTcxNzg2MzEzMnww&ixlib=rb-4.0.3&q=80&w=1080'};
+          console.log(error.message);
+          return {
+            novoCardData,
+            imageUrl:
+              "https://images.unsplash.com/photo-1557724630-96de91632c3b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2MTg2MTd8MHwxfHNlYXJjaHwxfHx1bmRlZmluZWR8ZW58MHwwfHx8MTcxNzg2MzEzMnww&ixlib=rb-4.0.3&q=80&w=1080",
+          };
         }
       })
       .then(({ novoCardData, imageUrl }) => {
@@ -88,7 +106,7 @@ export default function Cards(props) {
         if (error.response && error.response.status === 409) {
           alert(error.response.data);
         } else {
-          alert('Erro ao extrair dados da página');
+          alert("Erro ao extrair dados da página");
           console.log(error.response.data.message);
         }
       })
@@ -98,7 +116,7 @@ export default function Cards(props) {
   };
 
   const deletarCard = async (tag_raiz, id) => {
-    setLoadingData(true)
+    setLoadingData(true);
     try {
       await axios.delete(
         `https://websage-api.abelcode.dev/api/delete-item/${id}`,
@@ -106,27 +124,32 @@ export default function Cards(props) {
       );
       removerElemento(tag_raiz, id);
     } catch (error) {
-      alert('Item não foi apagado');
+      alert("Item não foi apagado");
     }
     setLoadingData(false);
   };
 
   const atualizaCard = async (ramo_id, imageUrl) => {
     await axios
-      .put(`https://websage-api.abelcode.dev/api/atualizar-item`, { imageUrl, ramo_id }, config).then(async (novoCard) => {
+      .put(
+        `https://websage-api.abelcode.dev/api/atualizar-item`,
+        { imageUrl, ramo_id },
+        config
+      )
+      .then(async (novoCard) => {
         await buscarImagem(novoCard.tag1, novoCard.tag2, novoCard.tag3);
       });
-  }
+  };
 
   // ** ------------------------------------ Operacoes em Array --------------------------------------
   const adicionarNovoElemento = (novoElemento, imageUrl) => {
     novoElemento.imageUrl = imageUrl;
-  
+
     // Verificar se há um elemento com a mesma tag_raiz
     const elementoExistente = tagsGerais.find(
       (item) => item.tag_raiz === novoElemento.tag_raiz
     );
-  
+
     // Se não houver, criar um novo elemento
     if (!elementoExistente) {
       const novoObjeto = {
@@ -145,19 +168,30 @@ export default function Cards(props) {
       });
       setTagsGerais(novosTagsGerais);
     }
+    // Atualiza os dados no localStorage
+    localStorage.setItem("cards", JSON.stringify({ data: tagsGerais }));
   };
 
   const removerElemento = (tagRaiz, id) => {
-    setTagsGerais(prevTags => {
-        return prevTags.map(item => {
-            if (item.tag_raiz === tagRaiz) {
-                const novosRamos = item.ramos.filter(ramo => ramo._id !== id);
-                return { ...item, ramos: novosRamos };
-            }
-            return item;
-        });
+    setTagsGerais((prevTags) => {
+      return prevTags.map((item) => {
+        if (item.tag_raiz === tagRaiz) {
+          const novosRamos = item.ramos.filter((ramo) => ramo._id !== id);
+          return { ...item, ramos: novosRamos };
+        }
+        return item;
+      });
     });
+    // Atualiza os dados no localStorage
+    localStorage.setItem("cards", JSON.stringify({ data: tagsGerais }));
   };
+
+  // Função para obter dados, seja do cache ou da API
+  function getCachedData() {
+    const cachedItems = localStorage.getItem('cards');
+    const parsedItems = JSON.parse(cachedItems);
+    return parsedItems?.data;
+  }
 
   return (
     <>
@@ -168,34 +202,34 @@ export default function Cards(props) {
               <div
                 style={{
                   backgroundImage: `url(${card.imageUrl})`,
-                  backgroundSize: 'cover',
-                  backgroundPosition: 'center',
-                  margin: '10px',
-                  borderRadius: '4.5px',
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                  margin: "10px",
+                  borderRadius: "4.5px",
                 }}
               >
                 <Card
                   className={styles.cardContainer}
-                  sx={{ display: 'flex', backgroundColor: '#ffffffd9' }}
+                  sx={{ display: "flex", backgroundColor: "#ffffffd9" }}
                 >
-                  <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                  <Box sx={{ display: "flex", flexDirection: "column" }}>
                     <a
                       href={card.url}
-                      target='_blank'
-                      rel='noopener noreferrer'
+                      target="_blank"
+                      rel="noopener noreferrer"
                     >
-                      <CardContent sx={{ flex: '1 0 auto' }}>
+                      <CardContent sx={{ flex: "1 0 auto" }}>
                         <Typography
-                          component='div'
-                          variant='h6'
-                          sx={{ paddingBottom: '10px', fontWeight: '500' }}
+                          component="div"
+                          variant="h6"
+                          sx={{ paddingBottom: "10px", fontWeight: "500" }}
                         >
                           {card.titulo}
                         </Typography>
                         <Typography
-                          variant='subtitle2'
-                          color='text.secondary'
-                          component='div'
+                          variant="subtitle2"
+                          color="text.secondary"
+                          component="div"
                         >
                           {card.descricao}
                         </Typography>
@@ -204,16 +238,16 @@ export default function Cards(props) {
                     <div className={styles.tags}>
                       <a
                         href={card.url}
-                        target='_blank'
-                        rel='noopener noreferrer'
+                        target="_blank"
+                        rel="noopener noreferrer"
                       >
                         <Box
                           sx={{
-                            display: 'flex',
-                            alignItems: 'center',
+                            display: "flex",
+                            alignItems: "center",
                             pl: 1,
                             pb: 1,
-                            gap: '10px',
+                            gap: "10px",
                           }}
                         >
                           {card.tag1 && <Chip label={card.tag1} />}
@@ -222,13 +256,13 @@ export default function Cards(props) {
                         </Box>
                       </a>
                       <IconButton
-                        z-index='1'
-                        aria-label='delete'
-                        size='small'
-                        color='error'
+                        z-index="1"
+                        aria-label="delete"
+                        size="small"
+                        color="error"
                         onClick={() => deletarCard(tags.tag_raiz, card._id)}
                       >
-                        <DeleteIcon fontSize='inherit' />
+                        <DeleteIcon fontSize="inherit" />
                       </IconButton>
                     </div>
                   </Box>
@@ -239,7 +273,7 @@ export default function Cards(props) {
         )}
       </div>
       <Backdrop
-        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
         open={loadingData}
       >
         <CircularProgress color="inherit" />
